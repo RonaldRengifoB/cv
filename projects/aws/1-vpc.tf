eec147c6cb900e47a93dbd13ba9b9d430ccb599f
Ronald Rengifo <dev@ronaldrengifo.com>
2024-05-04 13:54:22 -0500

    updated file structure

--- projects/aws/1-vpc.tf
+++
@@ -1,7 +0,0 @@
-#Create VPC
-resource "aws_vpc" "vpc" {
-  cidr_block = local.network.vpc_cidr
-  tags = {
-    Name = "${local.project.name}-vpc"
-  }
-}
--- projects/aws/3-igw.tf
+++
@@ -1,7 +0,0 @@
-#Create Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${local.project.name}-igw"
-    }
-}
